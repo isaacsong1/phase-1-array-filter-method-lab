e40eadb4910eea837c1dccec40a445eb51652179
Isaac Song <isaacsongs10@gmail.com>
2023-09-17 21:13:08 -0700

    filter lab complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(namesArr, string) {
+    return namesArr.filter(match => match.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(namesArr, string) {
+    return namesArr.filter(match => match[0] === string[0]);
+}
+
+function matchName(objArr, name) {
+    return objArr.filter(match => match.name === name);
+}
